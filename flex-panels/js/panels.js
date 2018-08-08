9b8cde2eac0ae9c1a1f25bafac9be23f05cd6a09
Michael Esch <eschmichael@yandex.com>
2018-08-08 18:50:29 -0400

    Use toggle, simplify css / js

--- flex-panels/js/panels.js
+++ flex-panels/js/panels.js
@@ -1,13 +1,16 @@
 const panels = document.querySelectorAll('.panel');
-const childern = document.querySelectorAll('.panel p');
 
-const selectPanel = (event) => {
-  const currentPanel = (event.target.className === '') ? event.target.parentNode : event.target;
-  panels.forEach( panel => panel.classList.remove('open'));
-  currentPanel.classList.add('open');
-  
-
+function toggleOpen () {
+  panels.forEach(panel => panel.classList.remove('open'));
+  this.classList.toggle('open');
 }
 
-panels.forEach( panel => panel.addEventListener('mousedown', selectPanel));
-childern.forEach( child => child.addEventListener('mousedown', selectPanel));
+function toggleActive (event) {
+  if (event.propertyName.includes('flex'))
+  this.classList.toggle('open-active');
+}
+
+panels.forEach( panel => {
+  panel.addEventListener('click', toggleOpen);
+  panel.addEventListener('transitionend', toggleActive);
+});
