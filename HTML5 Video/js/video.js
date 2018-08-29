7c841ed487fbe98ea3822381496bcd5072143b5a
Michael Esch <eschmichael@yandex.com>
2018-08-29 15:16:33 -0400

    HTML5 Video initial commit

--- HTML5 Video/js/video.js
+++ HTML5 Video/js/video.js
@@ -5,37 +5,79 @@ const playIndicator = document.querySelector('.play-indicator');
 let isPlaying = false;
 
 const hide = (...elements) => {
-  elements.forEach((element) => {
+  elements.forEach(element => {
     element.classList.add('hidden');
   });
 };
 const show = (...elements) => {
-  elements.forEach((element) => {
+  elements.forEach(element => {
     element.classList.remove('hidden');
   });
 };
 
 const play = () => {
-  const pause = '<i class="pause far fa-pause-circle"></i>';
-  const play = '<i class="play far fa-play-circle"></i>';
+  const pauseIcon = '<i class="pause far fa-pause-circle"></i>';
+  const playIcon = '<i class="play far fa-play-circle"></i>';
 
   if (!video.paused) {
     video.pause();
     isPlaying = false;
-    playIndicator.innerHTML = play;
+    playIndicator.innerHTML = playIcon;
     return;
   }
   video.play();
   isPlaying = true;
-  playIndicator.innerHTML = pause;
+  playIndicator.innerHTML = pauseIcon;
 };
 
+const updateProgress = () => {
+  const current = video.currentTime;
+  const position = Math.ceil((current / video.duration) * 100);
+  player.querySelector('progress').value = position;
+};
+
+function handleControl(event) {
+  let newTime = video.currentTime;
+  switch (event.target.dataset.ctrl) {
+    case 'fast-back':
+    // Set currentTime directly
+      video.currentTime = 0;
+      play();
+      break;
+    case 'step-back':
+      newTime -= 10;
+      video.currentTime = (newTime >= 0) ? newTime : 0;
+
+      break;
+    case 'step-forward':
+      newTime += 10;
+      video.currentTime = (newTime <= video.duration) ? newTime : video.duration;
+
+      break;
+    case 'fast-forward':
+      video.currentTime = video.duration;
+      break;
+    default:
+      break;
+  }
+
+}
+
 player.addEventListener('click', play);
-player.addEventListener('mouseover', (event) => {
+player.addEventListener('mouseover', event => {
   show(controls, playIndicator);
 });
-player.addEventListener('mouseleave', (event) => {
+player.addEventListener('mouseleave', event => {
   if (isPlaying) {
     hide(controls, playIndicator);
   }
 });
+video.addEventListener('timeupdate', updateProgress);
+document.addEventListener(
+  'DOMContentLoaded',
+  () => {
+    player.querySelector('progress').style.width = `${video.width}px`;
+  },
+  false,
+);
+controls.addEventListener('click', handleControl, true);
