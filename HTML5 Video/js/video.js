bcb5621edd69d319b76ad3f9068286c38b1e543d
Michael Esch <eschmichael@yandex.com>
2018-08-30 18:59:22 -0400

    full screen and click progress drag

--- HTML5 Video/js/video.js
+++ HTML5 Video/js/video.js
@@ -2,7 +2,9 @@ const controls = document.querySelector('.controls');
 const video = document.querySelector('video');
 const player = document.querySelector('.player');
 const playIndicator = document.querySelector('.play-indicator');
-let isPlaying = false;
+const playControl = controls.querySelector('.play');
+const progress = player.querySelector('progress');
+const ranges = controls.querySelector('input[type=range]');
 
 const hide = (...elements) => {
   elements.forEach(element => {
@@ -15,22 +17,35 @@ const show = (...elements) => {
   });
 };
 
-function play(event) {
-  const pauseIcon = '<i class="pause far fa-pause-circle"></i>';
-  const playIcon = '<i class="play far fa-play-circle"></i>';
-  console.dir(event.target);
-
-  if (!video.paused) {
-    video.pause();
-    isPlaying = false;
-    playIndicator.innerHTML = playIcon;
-    return;
+function togglePlay(event) {
+  if (event) {
+    console.info('play triggered');
+    console.dir(event.target);
   }
-  video.play();
-  isPlaying = true;
-  playIndicator.innerHTML = pauseIcon;
+  
+  const method = video.paused ? 'play' : 'pause';
+  video[method]();
 };
 
+function updatePlayBtn() {
+  const pauseIcon = '<i class="pause fa fa-pause" data-ctrl="pause"></i>';
+  const playIcon = '<i class="play fa fa-play" data-ctrl="play"></i>';
+
+  if (!video.paused) {
+    playIndicator.innerHTML = playIcon;
+    playControl.innerHTML = playIcon;
+    return;
+  }
+  playIndicator.innerHTML = pauseIcon;
+  playControl.innerHTML = pauseIcon;
+}
+
+
+function scrub(event) {
+  video.currentTime = (event.offsetX / progress.offsetWidth) * video.duration;
+  togglePlay();
+  updatePlayBtn();
+}
 const updateProgress = () => {
   const current = video.currentTime;
   const position = Math.ceil((current / video.duration) * 100);
@@ -40,11 +55,19 @@ const updateProgress = () => {
 function handleControl(event) {
   event.cancelBubble = (event.target.nodeName === 'I') ? true : false;
   let newTime = video.currentTime;
-  switch (event.target.dataset.ctrl) {
+  const control = (!event.target.dataset.ctrl) ? 'play' : event.target.dataset.ctrl;
+
+  switch (control) {
+    case 'play':
+      togglePlay();
+      break;
+    case 'pause':
+      togglePlay();
+      break;
     case 'fast-back':
     // Set currentTime directly
       video.currentTime = 0;
-      play();
+      togglePlay();
       break;
     case 'step-back':
       newTime -= 10;
@@ -59,21 +82,53 @@ function handleControl(event) {
     case 'fast-forward':
       video.currentTime = video.duration;
       break;
+    case 'volume':
+      video.volume = event.target.value;
+      break;
+    case 'fullscreen':
+      toggleFullscreen();
+      break;
     default:
       break;
   }
 
 }
 
-player.addEventListener('click', play);
+function toggleFullscreen() {
+  if (isFullScreen()) {
+    if (document.exitFullscreen) document.exitFullscreen();
+    else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+    else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
+    else if (document.msExitFullscreen) document.msExitFullscreen();
+    setFullscreenData(false);
+ }
+ else {
+    if (video.requestFullscreen) video.requestFullscreen();
+    else if (video.mozRequestFullScreen) video.mozRequestFullScreen();
+    else if (video.webkitRequestFullScreen) video.webkitRequestFullScreen();
+    else if (video.msRequestFullscreen) video.msRequestFullscreen();
+    setFullscreenData(true);
+ }
+}
+
+const isFullScreen = function() {
+  return !!(document.fullScreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
+}
+
+player.addEventListener('click', handleControl);
 player.addEventListener('mouseover', event => {
+  progress.classList.add('show');
   show(controls, playIndicator);
 });
 player.addEventListener('mouseleave', event => {
-  if (isPlaying) {
+  if (!video.paused) {
+    progress.classList.remove('show');
     hide(controls, playIndicator);
   }
 });
+
+video.addEventListener('play', updatePlayBtn);
+video.addEventListener('pause', updatePlayBtn);
 video.addEventListener('timeupdate', updateProgress);
 document.addEventListener(
   'DOMContentLoaded',
@@ -82,4 +137,9 @@ document.addEventListener(
   },
   false,
 );
-controls.addEventListener('click', handleControl, true);
+controls.addEventListener('change', handleControl, true);
+progress.addEventListener('click', scrub);
+let mousedown = false;
+progress.addEventListener('mousemove', (event) => mousedown && scrub(event));
+progress.addEventListener('mousedown', () => mousedown = true);
+progress.addEventListener('mouseup', () => mousedown = false);
